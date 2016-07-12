bc099a88720b7ece6764891551bbc0791930563c
Nils Kuhn <nils.kuhn@iteratec.de>
2016-07-12 23:27:42 +0200

    added docker stuff (copied and adapted from osm)

---
+++ docker/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# prepare osm config respective set env variables
+OSM_DA_CONFIG_TARGET_LOCATION="${OSM_DA_CONFIG_HOME}/OsmDetailAnalysis-config.yml"
+
+echo "complete osm detail analysis config file gets prepared"
+dockerize -template $OSM_DA_CONFIG_TARGET_LOCATION.j2:$OSM_DA_CONFIG_TARGET_LOCATION
+
+# start application
+java -Dgrails.env=prod -jar $OSM_DA_HOME/OsmDetailAnalysis.war
