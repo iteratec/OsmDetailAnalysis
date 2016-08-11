72e54dbd205d64d20f936cc7b7d5bfc9cfa4284f
Marko Schnecke <marko.schnecke@iteratec.de>
2016-08-11 12:21:26 +0200

    fixed bug

--- grails-app/services/de/iteratec/osm/da/persistence/AssetRequestPersistenceService.groovy
+++ grails-app/services/de/iteratec/osm/da/persistence/AssetRequestPersistenceService.groovy
@@ -71,7 +71,7 @@ class AssetRequestPersistenceService {
         aggregateList << match(and(matchList))
         aggregateList << unwind("\$assets")
         aggregateList << project(createPreFilterProjectDocument())
-        aggregateList << group(['jobId':'\$jobId','mediaType':'\$mediaType','browser':'\$browser','subtype':'\$subtype','epochTimeComplete':'\$epochTimeComplete'] as BasicDBObject,avg('loadTimeMs_avg','\$loadTimeMs'),min('loadTimeMs_min','\$loadTimeMs'),max('loadTimeMs_max','\$loadTimeMs'))
+        aggregateList << group(['jobId':'\$jobId','mediaType':'\$mediaType','browser':'\$browser','subtype':'\$subtype','epochTimeCompleted':'\$epochTimeCompleted'] as BasicDBObject,avg('loadTimeMs_avg','\$loadTimeMs'),min('loadTimeMs_min','\$loadTimeMs'),max('loadTimeMs_max','\$loadTimeMs'))
         aggregateList << project(createUnpackIdProjectDocument())
         return JsonOutput.toJson(db.getCollection("assetRequestGroup").aggregate(aggregateList).allowDiskUse(true))
     }
