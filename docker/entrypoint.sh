a2015c3d56875254399945c340d79e8aff02b91a
Nils Kuhn <nils.kuhn@iteratec.de>
2016-10-10 17:20:02 +0200

    wip: dockerization

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,8 +9,5 @@ dockerize -template $OSM_DA_CONFIG_TARGET_LOCATION.j2:$OSM_DA_CONFIG_TARGET_LOCA
 echo "define stdout and stderr"
 dockerize -stdout ${OSM_DA_HOME}/logs/OsmDetailAnalysisDetails.log -stderr ${OSM_DA_HOME}/logs/OsmDetailAnalysis.log
 
-echo "wait for mongodb container to run"
-dockerize -wait tcp://osm_mongodb:27017 -timeout 240s
-
 echo "start application"
 java -Dgrails.env=prod -Xms$OSM_JVM_XMS -Xmx$OSM_JVM_XMX -jar $OSM_DA_HOME/OsmDetailAnalysis*.war
