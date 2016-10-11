d9918e4cf4a2954a091970b50a0919b1361546e4
Nils Kuhn <nils.kuhn@iteratec.de>
2016-10-11 10:55:03 +0200

    wip: dockerization

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "start application"
-java -Dgrails.env=prod -Xms$OSM_JVM_XMS -Xmx$OSM_JVM_XMX -jar $OSM_DA_HOME/OsmDetailAnalysis*.war
+java -server -Dgrails.env=prod -Dfile.encoding=UTF-8 -Xms${OSMDA_JVM_XMS} -Xmx${OSMDA_JVM_XMX} -jar ${OSM_DA_HOME}/OsmDetailAnalysis*.war
--- docker/startApp.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-export PUBLIC_PORT=8081
-export OSM_JVM_XMS=1024m
-export OSM_JVM_XMX=4096m
-docker-compose up -d
