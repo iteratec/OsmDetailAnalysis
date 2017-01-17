6ed65631a4d8f23ac3c5ef2018339a47e689fc17
Nils Kuhn <nils.kuhn@iteratec.de>
2017-01-17 17:25:40 +0100

    wip: running compose deployment via Jenkinsfile

--- scripts/ci/deploy/run_compose_deployment.sh
+++ scripts/ci/deploy/run_compose_deployment.sh
@@ -7,11 +7,13 @@ echo "stderr_file=${stderr_file}"
 echo "run compose deployment on target host"
 echo "####################################################"
 ssh -p $osmda_host_ssh_port -o "StrictHostKeyChecking no" $osmda_host_os_username@$osmda_host /bin/bash <<EOT
-cd $COMPOSE_BIN_FOLDER
-echo "$SEU_JENKINS_ITERATEC_AT_OSM_HH_ITERATEC_DE" | sudo -S docker-compose stop 2> /tmp/${stderr_file}
-echo "$SEU_JENKINS_ITERATEC_AT_OSM_HH_ITERATEC_DE" | sudo -S docker-compose rm -f 2> /tmp/${stderr_file}
-echo "$SEU_JENKINS_ITERATEC_AT_OSM_HH_ITERATEC_DE" | sudo -S docker-compose pull 2> /tmp/${stderr_file}
-echo "$SEU_JENKINS_ITERATEC_AT_OSM_HH_ITERATEC_DE" | sudo -S docker-compose up -d 2> /tmp/${stderr_file}
+pwd
+ls -la
+#cd $COMPOSE_BIN_FOLDER
+#echo "$SEU_JENKINS_ITERATEC_AT_OSM_HH_ITERATEC_DE" | sudo -S docker-compose stop 2> /tmp/${stderr_file}
+#echo "$SEU_JENKINS_ITERATEC_AT_OSM_HH_ITERATEC_DE" | sudo -S docker-compose rm -f 2> /tmp/${stderr_file}
+#echo "$SEU_JENKINS_ITERATEC_AT_OSM_HH_ITERATEC_DE" | sudo -S docker-compose pull 2> /tmp/${stderr_file}
+#echo "$SEU_JENKINS_ITERATEC_AT_OSM_HH_ITERATEC_DE" | sudo -S docker-compose up -d 2> /tmp/${stderr_file}
 EOT
 
 echo "get stderr from remote host"
