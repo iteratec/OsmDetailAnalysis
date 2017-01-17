f62fd4033d6e7c6752f54cf24bb5369b1c4a78af
Nils Kuhn <nils.kuhn@iteratec.de>
2017-01-17 16:03:48 +0100

    Jenkins file deployment: added further apiKey config entry

--- scripts/ci/deploy/prepare_compose_env_file.sh
+++ scripts/ci/deploy/prepare_compose_env_file.sh
@@ -4,11 +4,18 @@ COMPOSE_BIN_FOLDER="~/OsmDetailAnalysis"
 
 echo "prepare environment-vars file for compose"
 echo "####################################################"
+
 echo "OSMDA_MONGODB_HOST=osm_mongodb" > ./docker/.env-osmda
+
 echo "OSMDA_INITIAL_API_KEY=$OSMDA_API_KEY_PROD_ITERATEC" >> ./docker/.env-osmda
 echo "OSMDA_INITIAL_OSM_URL=http://openspeedmonitor.wpt.iteratec.de" >> ./docker/.env-osmda
+
 echo "OSMDA_INITIAL_API_KEY1=$OSMDA_API_KEY_DEV_ITERATEC" >> ./docker/.env-osmda
 echo "OSMDA_INITIAL_OSM_URL1=http://dev.openspeedmonitor.wpt.iteratec.de" >> ./docker/.env-osmda
+
+echo "OSMDA_INITIAL_API_KEY2=$OSMDA_API_KEY_OTTO" >> ./docker/.env-osmda
+echo "OSMDA_INITIAL_OSM_URL2=https://iteraspeedmonitor.otto.wpt.iteratec.de" >> ./docker/.env-osmda
+
 echo "OSMDA_JVM_XMS=1024m" >> ./docker/.env-osmda
 echo "OSMDA_JVM_XMX=8192m" >> ./docker/.env-osmda
 
