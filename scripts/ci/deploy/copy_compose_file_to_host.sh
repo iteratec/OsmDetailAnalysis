6e0766ccc65cf0f6d2e245a3229401b5d5020163
Nils Kuhn <nils.kuhn@iteratec.de>
2017-01-17 14:20:12 +0100

    wip: deployment via Jenkinsfile

--- scripts/ci/deploy/copy_compose_file_to_host.sh
+++ scripts/ci/deploy/copy_compose_file_to_host.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env bash
-set -e
+# set -e
 
-COMPOSE_BIN_FOLDER="/opt/OsmDetailAnalysis"
-stderr_file="osm_${bamboo_OsmDetailAnalysis_Host}_stderr.txt"
+COMPOSE_BIN_FOLDER="~/OsmDetailAnalysis"
+stderr_file="osm_${osmda_host}_stderr.txt"
+echo "stderr_file=${stderr_file}"
 
 echo "create directory for OsmDetailAnalysis on target host"
 echo "####################################################"
-echo "create directory"
-ssh -p $bamboo_OsmDetailAnalysis_Port -o "StrictHostKeyChecking no" $bamboo_OsmDetailAnalysis_OsUsername@$bamboo_OsmDetailAnalysis_Host /bin/bash <<EOT
-if [ ! -d "$COMPOSE_BIN_FOLDER" ]; then mkdir $COMPOSE_BIN_FOLDER fi 2> /tmp/${stderr_file}
+echo "-> create directory $COMPOSE_BIN_FOLDER"
+ssh -p $osmda_host_ssh_port -o "StrictHostKeyChecking no" $osmda_host_os_username@$osmda_host /bin/bash <<EOT
+if [ ! -d "$COMPOSE_BIN_FOLDER" ]; then
+	mkdir $COMPOSE_BIN_FOLDER
+fi 2> /tmp/${stderr_file}
 EOT
+
 echo "get stderr from remote host"
-scp -P $bamboo_OsmDetailAnalysis_Port -o "StrictHostKeyChecking no" $bamboo_OsmDetailAnalysis_OsUsername@$bamboo_OsmDetailAnalysis_Host:"/tmp/$stderr_file" .
-echo "stderr create folder START"
+scp -P $osmda_host_ssh_port -o "StrictHostKeyChecking no" $osmda_host_os_username@$osmda_host:"/tmp/$stderr_file" .
+echo "-> stderr create folder START"
 cat "./$stderr_file"
-echo "stderr create folder END"
+echo "-> stderr create folder END"
 
 echo "copy compose file to target host"
 echo "####################################################"
-scp -P $bamboo_OsmDetailAnalysis_Port -o "StrictHostKeyChecking no" ./OsmDetailAnalysis/docker/docker-compose.yml $bamboo_OsmDetailAnalysis_OsUsername@$bamboo_OsmDetailAnalysis_Host:"${COMPOSE_BIN_FOLDER}/docker-compose.yml"
+scp -P $osmda_host_ssh_port -o "StrictHostKeyChecking no" ./docker/docker-compose.yml $osmda_host_os_username@$osmda_host:"${COMPOSE_BIN_FOLDER}/"
