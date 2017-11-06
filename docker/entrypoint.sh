c01128c3e0ed97e6bf9b9d782b936aec00e7038a
Nils Kuhn <nils.kuhn@iteratec.de>
2017-11-06 22:22:21 +0100

    added possibility to set keystore on startup in container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-echo "start application"
-java -server -Dgrails.env=prod -Dfile.encoding=UTF-8 -Xms${OSMDA_JVM_XMS} -Xmx${OSMDA_JVM_XMX} -jar ${OSM_DA_HOME}/OsmDetailAnalysis.war
+if [ -z "$KEYSTORE" ] || [ -z "$KEYSTORE_PASSWORD" ]; then
+    # start without keystore
+    java -server -Dgrails.env=prod \
+        -Dfile.encoding=UTF-8 \
+        -Xms${OSMDA_JVM_XMS} -Xmx${OSMDA_JVM_XMX} \
+        -jar ${OSM_DA_HOME}/OsmDetailAnalysis.war
+else
+    # start with keystore
+    java -server -Dgrails.env=prod \
+        -Dfile.encoding=UTF-8 \
+        -Djavax.net.ssl.trustStore=${KEYSTORE} \
+        -Djavax.net.ssl.trustStorePassword=${KEYSTORE_PASSWORD} \
+        -Xms${OSMDA_JVM_XMS} -Xmx${OSMDA_JVM_XMX} \
+        -jar ${OSM_DA_HOME}/OsmDetailAnalysis.warelse
+fi
